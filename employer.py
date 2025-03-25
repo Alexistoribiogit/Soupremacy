e6d94b10e57ba5e05749f9ebc5ea3df900040d8b
tmanelle <manelle.tahiri@ynov.com>
2025-03-25 17:12:59 +0100

    amelioration du test et class employer

--- employer.py
+++ employer.py
@@ -2,3 +2,10 @@ class Employer:
     def __init__(self):
         self.occupe = False
         self.employe = None
+
+    def embauche(self):
+        self.occupe = True
+
+    def licencie(self):
+        self.occupe = False
+        self.employe = None
--- test/test_employer.py
+++ test/test_employer.py
@@ -2,4 +2,18 @@ from employer import Employer
 
 
 def test_employer_initialement_libre():
-    pass
+    employer = Employer()
+    assert employer.occupe == False
+
+
+def test_employer_embauche():
+    employer = Employer()
+    employer.embauche()
+    assert employer.occupe == True
+
+
+def test_employer_licencie():
+    employer = Employer()
+    employer.embauche()
+    employer.licencie()
+    assert employer.occupe == False

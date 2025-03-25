66d21760821fac6cd1727121cf314355c8cbe32e
tmanelle <manelle.tahiri@ynov.com>
2025-03-25 17:12:31 +0100

    modif de legs passé de patate a courgette

--- test/test_champ.py
+++ test/test_champ.py
@@ -9,7 +9,7 @@ def test_champ_semable():
 
 def test_champ_non_semable():
     champ = Champ()
-    champ.semer(Legume.PATATE)
+    champ.semer(Legume.COURGETTE)
     assert champ.semable() == False
 
 
@@ -20,13 +20,13 @@ def test_champ_vide_non_arrosable():
 
 def test_champ_juste_semer_non_arrosable():
     champ = Champ()
-    champ.semer(Legume.PATATE)
+    champ.semer(Legume.COURGETTE)
     assert champ.arrosable() == True
 
 
 def test_champ_arroser():
     champ = Champ()
-    champ.semer(Legume.PATATE)
+    champ.semer(Legume.COURGETTE)
     for _ in range(10):
         champ.arroser()
     assert champ.arrosable() == False
@@ -34,7 +34,7 @@ def test_champ_arroser():
 
 def test_champ_arroser_mais_pas_assez():
     champ = Champ()
-    champ.semer(Legume.PATATE)
+    champ.semer(Legume.COURGETTE)
     for _ in range(9):
         champ.arroser()
     assert champ.arrosable() == True

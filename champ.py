c11d44672e47864dd51d1fe47565eddd7c357328
tmanelle <manelle.tahiri@ynov.com>
2025-03-25 10:07:56 +0100

    ajout legume dans test_champ

--- champ.py
+++ champ.py
@@ -1,3 +1,9 @@
 class Champ:
+    def __init__(self):
+        self.legume = None
+
     def semable(self):
-        return True
+        return self.legume is None
+
+    def semer(self, legume):
+        self.legume = legume
--- test/test_champ.py
+++ test/test_champ.py
@@ -1,5 +1,13 @@
 from champ import Champ
+from legume import Legume
+
 
 def test_champ_semable():
     champ = Champ()
     assert champ.semable() == True
+
+
+def test_champ_non_semable():
+    champ = Champ()
+    champ.semer(Legume.PATATE)
+    assert champ.semable() == False

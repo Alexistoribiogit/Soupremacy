db876628c785e1682f5f9c3f3a72c301a0118380
sleopaul <leopaul.sable@ynov.com>
2025-03-25 12:44:18 +0100

    ajout legumes

--- legume.py
+++ legume.py
@@ -3,3 +3,9 @@ from enum import StrEnum
 
 class Legume(StrEnum):
     PATATE = "patate"
+    POIREAU = "poireau"
+    TOMATE = "tomate"
+    OIGNON = "oignon"
+    COURGETTE = "courgette"
+    
+    
